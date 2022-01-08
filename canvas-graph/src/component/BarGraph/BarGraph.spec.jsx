d895f2bfdb2e0ad12964bd02f701eb4f0133f279
Gopal Ojha <gopal.ojha.1989@gmail.com>
2022-01-08 17:36:24 +0530

    feat(canvas): add visual/functional tests

--- canvas-graph/src/component/BarGraph/BarGraph.spec.jsx
+++ canvas-graph/src/component/BarGraph/BarGraph.spec.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
-import { mount } from '@cypress/react'
+import { mount, mountHook } from '@cypress/react'
 import BarGraph from './BarGraph'
 import Provider from '../Theme/Provider'
+import useData from './useData'
 
 const data = [
     { sales: 14, month: 'Jan' },
-    { sales: 18, month: 'Feb' },
+    { sales: 10, month: 'Feb' },
     { sales: 16, month: 'Mar' },
     { sales: 9, month: 'Apr' },
     { sales: 12, month: 'May' },
@@ -17,59 +18,104 @@ const props = {
     xKey: 'month',
     yUnit: 'M',
 }
+const dims = {
+    padding: 40,
+    canvasH: 500,
+    canvasW: 500,
+}
 
-it('should render Bar graph', () => {
-    mount(
-        <Provider>
-            <BarGraph {...props} />
-        </Provider>
-    )
+describe('functional tests suite', () => {
+    it('should calculate correct values of min/max y', () => {
+        mountHook(() => useData(data, props.xKey, props.yKey, dims)).then(
+            (result) => {
+                expect(result.current.minY).to.equal(0) // min value is zero irrespective of data min
+                expect(result.current.maxY).to.equal(16)
+            }
+        )
+    })
+
+    it('should have the same data length', () => {
+        mountHook(() => useData(data, props.xKey, props.yKey, dims)).then(
+            (result) => {
+                expect(result.current.graphData.length).to.equal(data.length)
+            }
+        )
+    })
+
+    it('should not have undefined and null values', () => {
+        mountHook(() => useData(data, props.xKey, props.yKey, dims)).then(
+            (result) => {
+                result.current.graphData.forEach((dataSet) =>
+                    expect(
+                        Object.values(dataSet).filter(Boolean).length
+                    ).to.equal(4)
+                )
+            }
+        )
+    })
+    // figure a way to compare the actual output of graph data, from the hook
+    // all negative flows
+    it('should return empty array for no data', () => {
+        mountHook(() => useData()).then((result) => {
+            expect(result.current.graphData.length).to.equal(0)
+            expect(result.current.minY).to.equal(0)
+            expect(result.current.maxY).to.equal(0)
+        })
+    })
 })
 
-it('should render light mode', () => {
-    mount(
-        <Provider mode="light">
-            <BarGraph {...props} />
-        </Provider>
-    )
-    cy.get('[data-testid="bar-wrapper"]').should(
-        'have.css',
-        'background-color',
-        'rgb(253, 249, 243)'
-    )
-})
+describe('visual tests suite', () => {
+    it('should render light mode', () => {
+        mount(
+            <Provider mode="light">
+                <BarGraph {...props} />
+            </Provider>
+        )
+        cy.get('[data-testid="bar-wrapper"]').should(
+            'have.css',
+            'background-color',
+            'rgb(253, 249, 243)'
+        )
+        cy.wait(500)
+        cy.percySnapshot()
+    })
 
-it('should render dark mode', () => {
-    mount(
-        <Provider mode="dark">
-            <BarGraph {...props} />
-        </Provider>
-    )
-    cy.get('[data-testid="bar-wrapper"]').should(
-        'have.css',
-        'background-color',
-        'rgb(36, 36, 36)'
-    )
-})
+    it('should render dark mode', () => {
+        mount(
+            <Provider mode="dark">
+                <BarGraph {...props} />
+            </Provider>
+        )
+        cy.get('[data-testid="bar-wrapper"]').should(
+            'have.css',
+            'background-color',
+            'rgb(36, 36, 36)'
+        )
+        cy.wait(500)
+        cy.percySnapshot()
+    })
 
-it('should show correct sales figure', () => {
-    mount(
-        <Provider>
-            <BarGraph {...props} />
-        </Provider>
-    )
-    cy.get('[data-testid="bar-sales-figure"]').contains(
-        data[data.length - 1].sales + props.yUnit
-    )
-})
+    it('should show correct sales figure', () => {
+        mount(
+            <Provider>
+                <BarGraph {...props} />
+            </Provider>
+        )
+        cy.get('[data-testid="bar-sales-figure"]').contains(
+            data[data.length - 1].sales + props.yUnit
+        )
+    })
 
-it('should show correct delta figure', () => {
-    mount(
-        <Provider>
-            <BarGraph {...props} />
-        </Provider>
-    )
-    cy.get('[data-testid="bar-delta-figure"]').contains(
-        data[data.length - 1].sales - data[data.length - 2].sales + props.yUnit
-    )
+    it('should show correct delta figure', () => {
+        mount(
+            <Provider>
+                <BarGraph {...props} />
+            </Provider>
+        )
+        cy.get('[data-testid="bar-delta-figure"]').contains(
+            data[data.length - 1].sales -
+                data[data.length - 2].sales +
+                props.yUnit
+        )
+    })
 })
--- canvas-graph/src/component/BarGraph/useData.js
+++ canvas-graph/src/component/BarGraph/useData.js
@@ -1,63 +1,63 @@
-import React from "react";
-import { map } from "../../Utils/Utils";
+import React from 'react'
+import { map } from '../../Utils/Utils'
 
-function useData(data, xKey, yKey, dims) {
-  const [graphData, setGraphData] = React.useState([]);
-  const [minY, setMinY] = React.useState(0);
-  const [maxY, setMaxY] = React.useState(0);
+function useData(data = [], xKey = '', yKey = '', dims = {}) {
+    const [graphData, setGraphData] = React.useState([])
+    const [minY, setMinY] = React.useState(0)
+    const [maxY, setMaxY] = React.useState(0)
 
-  const range = React.useCallback((data, key) =>
-    data.reduce((acc, point, i) => {
-      if (i === 0) {
-        return { min: 0, max: point[key] };
-      }
-      return {
-        min: Math.min(acc.min, point[key]),
-        max: Math.max(acc.max, point[key]),
-      };
-    }, {})
-  );
+    const range = React.useCallback((data, key) =>
+        data.reduce((acc, point, i) => {
+            if (i === 0) {
+                return { min: 0, max: point[key] }
+            }
+            return {
+                min: Math.min(acc.min, point[key]),
+                max: Math.max(acc.max, point[key]),
+            }
+        }, {})
+    )
 
-  const transposeData = React.useCallback((data) => {
-    return data.map((point) => ({
-      ...point,
-      y: dims.canvasH - point.y,
-    }));
-  });
+    const transposeData = React.useCallback((data) => {
+        return data.map((point) => ({
+            ...point,
+            y: dims.canvasH - point.y,
+        }))
+    })
 
-  const transformData = React.useCallback(() => {
-    const { min, max } = range(data, yKey);
-    const newData = data.map((dataPoints, index) => ({
-      x: map(
-        index,
-        0,
-        data.length - 1,
-        dims.padding * 2,
-        dims.canvasW - dims.padding * 2
-      ),
-      y: map(
-        dataPoints[yKey],
-        min,
-        max,
-        dims.padding,
-        dims.canvasH - dims.padding * 2
-      ),
-      xLegend: dataPoints[xKey],
-      yLegend: dataPoints[yKey],
-    }));
+    const transformData = React.useCallback(() => {
+        const { min, max } = range(data, yKey)
+        const newData = data.map((dataPoints, index) => ({
+            x: map(
+                index,
+                0,
+                data.length - 1,
+                dims.padding * 2,
+                dims.canvasW - dims.padding * 2
+            ),
+            y: map(
+                dataPoints[yKey],
+                min,
+                max,
+                dims.padding,
+                dims.canvasH - dims.padding * 2
+            ),
+            xLegend: dataPoints[xKey],
+            yLegend: dataPoints[yKey],
+        }))
 
-    setGraphData(transposeData(newData));
-    setMinY(min);
-    setMaxY(max);
-  });
+        setGraphData(transposeData(newData))
+        setMinY(min)
+        setMaxY(max)
+    })
 
-  React.useEffect(() => {
-    if (dims.canvasW && dims.canvasH) {
-      transformData();
-    }
-  }, [data, dims]);
+    React.useEffect(() => {
+        if (dims.canvasW && dims.canvasH) {
+            transformData()
+        }
+    }, [data, dims])
 
-  return { graphData, minY, maxY };
+    return { graphData, minY, maxY }
 }
 
-export default useData;
+export default useData
