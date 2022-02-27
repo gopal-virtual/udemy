49d1923ab069ea8b16918ff531821aec3ef6bff3
Gopal Ojha <gopal.ojha.1989@gmail.com>
2022-02-27 22:30:16 +0530

    enhance: add system theme change listener

--- live-content/bar-graph/src/components/Theme/Provider.jsx
+++ live-content/bar-graph/src/components/Theme/Provider.jsx
@@ -11,8 +11,23 @@ const GlobalStyle = createGlobalStyle`
 `
 
 function Provider({ children, mode = 'light' }) {
+    const [localMode, setLocalMode] = React.useState(mode)
+    const handleModeChange = React.useCallback((e) => {
+        setLocalMode(e.matches ? 'dark' : 'light')
+    })
+
+    React.useEffect(() => {
+        window
+            .matchMedia('(prefers-color-scheme: dark)')
+            .addEventListener('change', handleModeChange)
+
+        return window
+            .matchMedia('(prefers-color-scheme: dark)')
+            .removeEventListener('change', handleModeChange)
+    }, [])
+
     return (
-        <ThemeProvider theme={{ colors: theme.colors[mode] }}>
+        <ThemeProvider theme={{ colors: theme.colors[localMode] }}>
             {children}
             <GlobalStyle />
         </ThemeProvider>
